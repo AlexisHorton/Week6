2e1a47713b0dec1335d5518d9dab545cfee60022
alexis <alexisd.horton@gmail.com>
2021-12-21 11:54:54 -0500

    Latest

---
+++ MVCDemo2/Controllers/ShopController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo2.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         //Show product form  //shop.addproductform
+        public IActionResult AddProductForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //Add product after form is submitted: /shop/addproduct
+        public IActionResult AddProduct(Item theitem)
+        {
+            Item.Items.Add(theitem);
+            return Redirect("/");
+        }
+
+        public IActionResult DeleteItem(int id)
+        {
+            Item found = null;
+            foreach (Item current in Item.Items)
+            {
+                if (current.id == id)
+                {
+                    found = current;
+                }
+            }
+            if (found != null)
+            {
+                Item.Items.Remove(found);
+            }
+            return Redirect("/");
+        }
+
+        public IActionResult EditProductForm(int id)
+        {
+            Item found = null;
+            foreach (Item current in Item.Items)
+            {
+                if (current.id == id)
+                {
+                    found = current;
+                }
+            }
+            if (found != null)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+        public IActionResult EditProduct(Item theitem)
+        {
+            foreach (Item current in Item.Items)
+            {
+                if (current.id == theitem.id)
+                {
+                    current.Name = theitem.Name;
+                    current.Price = theitem.Price;
+                    break;
+                }
+            }
+            return Redirect("/");
+        }
+    }
+}
---
+++ MVCDemo2/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo2.Models
+{
+    public class Item
+    {
+        public static List<Item> Items = new List<Item>();
+
+        public int id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id {id} Name {Name} Price {Price}";
+        }
+    }
+}
